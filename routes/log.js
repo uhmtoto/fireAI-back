73a0300de4b2312a7d0f694eefe398d70094ac2f
uhmtoto <uhmtotow@gmail.com>
2019-05-13 12:29:50 +0900

    fix: add missing verifyPermission when get sensor

--- routes/log.js
+++ routes/log.js
@@ -2,8 +2,11 @@ var express = require('express')
 var router = express.Router()
 var Log = require('../models/log')
 var Error = require('../models/error')
+var verifyPermission = require('../src/verify/permission')
 
 router.get('/type/:sensorType', (req, res) => {
+  if (verifyPermission(req, res, 0)) return
+
   Log.find({}, (err, logs) => {
     if (err) res.status(500).json(Error('DB에 문제가 발생했습니다.'))
     res.json(logs.map)
