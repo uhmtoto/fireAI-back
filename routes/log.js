f00a19ea228190fe719d82de84df41c3cdfeed01
uhmtoto <uhmtotow@gmail.com>
2019-05-13 12:33:50 +0900

    fix(typo): remove map method in [GET] /log/type/:sensorType

--- routes/log.js
+++ routes/log.js
@@ -9,7 +9,7 @@ router.get('/type/:sensorType', (req, res) => {
 
   Log.find({}, (err, logs) => {
     if (err) res.status(500).json(Error('DB에 문제가 발생했습니다.'))
-    res.json(logs.map)
+    res.json(logs)
   })
 })
 
