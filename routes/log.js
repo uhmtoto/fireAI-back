473f762128661d59217fed7032329fc20b4814b1
uhmtoto <uhmtotow@gmail.com>
2019-05-14 14:44:05 +0900

    feat: add log router [GET] /log, [POST] /log

--- routes/log.js
+++ routes/log.js
@@ -3,6 +3,31 @@ var router = express.Router()
 var Log = require('../models/log')
 var Error = require('../models/error')
 var verifyPermission = require('../src/verify/permission')
+var verifyEmpty = require('../src/verify/empty')
+
+router.get('/', (req, res) => {
+  if (verifyPermission(req, res, 0)) return
+
+  Log.find({}, (err, logs) => {
+    if (err) return res.status(500).json(Error('DB에 에러가 발생했습니다.'))
+    res.json(logs)
+  })
+})
+
+router.post('/', (req, res) => {
+  if (verifyPermission(req, res, 3)) return
+  if (verifyEmpty(res, req.body, ['logs'])) return
+  var logs = req.body.logs
+
+  logs.forEach(v => {
+    var newLog = new Log()
+    newLog = Object.assign(newLog, v)
+    newLog.save(err => {
+      if (err) return res.status(500).json(Error('DB에 에러가 발생했습니다.'))
+    })
+    res.json({ success: true })
+  })
+})
 
 router.get('/type/:sensorType', (req, res) => {
   if (verifyPermission(req, res, 0)) return
