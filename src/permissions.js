92dc7aba0c4fa45691295c2d453e591cb6367d6d
uhmtoto <uhmtotow@gmail.com>
2019-05-14 14:45:14 +0900

    refactor: change permission value of admin

--- src/permissions.js
+++ src/permissions.js
@@ -1,6 +1,6 @@
 const permissions = [
   { name: 'sensor', permission: 3 },
-  { name: 'admin', permission: 2 },
+  { name: 'admin', permission: 3 },
   { name: 'fireman', permission: 1 },
   { name: 'user', permission: 0 }
 ]
