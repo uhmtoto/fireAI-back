5370c7ae3fdd3cb8c3de4b740adfb7d3f21455db
uhmtoto <uhmtotow@gmail.com>
2019-05-14 14:44:57 +0900

    refactor: change payload of log router

--- routes/sensor.js
+++ routes/sensor.js
@@ -26,35 +26,12 @@ router.get('/:sensorId', (req, res) => {
 router.post('/', (req, res) => {
   const formData = req.body
   if (verifyPermission(req, res, 2)) return
-  if (verifyEmpty(res, formData, ['type', 'value', 'sensor', 'time'])) return
+  if (verifyEmpty(res, formData, ['room', 'type', 'name'])) return
 
   var newSensor = new Sensor()
   newSensor = Object.assign(newSensor, formData)
-  newSensor.save(err => {
-    if (err) res.status(500).json(Error('DB에 오류가 발생했습니다.'))
-  })
-  res.json({ success: true })
-})
-
-router.get('/log', (req, res) => {
-  if (verifyPermission(req, res, 0)) return
-
-  Log.find({}, (err, logs) => {
-    if (err) return res.status(500).json(Error('DB에 에러가 발생했습니다.'))
-    res.json(logs)
-  })
-})
-
-router.post('/log', (req, res) => {
-  if (verifyPermission(req, res, 3)) return
-  var logs = req.body.logs
-
-  logs.forEach(v => {
-    var newLog = new Log()
-    newLog = Object.assign(newLog, v)
-    newLog.save(err => {
-      if (err) return res.status(500).json(Error('DB에 에러가 발생했습니다.'))
-    })
+  newSensor.save(async err => {
+    if (err) await res.status(500).json(Error('DB에 오류가 발생했습니다.'))
   })
   res.json({ success: true })
 })
